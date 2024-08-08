5f6a7bb6c6b7bf7076beb7ed4d8d4633a5725707
Amanda Venancio <aalmeidaven@gmail.com>
2024-08-07 23:18:35 -0300

    First commit

---
+++ src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -0,0 +1,18 @@
+package main.java.one.digitalinnovation.gof.service;
+
+import main.java.one.digitalinnovation.gof.model.Cliente;
+
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
